24782ba2ea195567274ae5b187debcca41a64351
Jose Ricaurte <jose.ricaurte99@gmail.com>
2025-02-09 08:16:23 -0500

    feat: create commons-lib

---
+++ projects/commons-lib/src/lib/commons-lib.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-commons-lib',
+  standalone: false,
+  template: `
+    <p>
+      commons-lib works!
+    </p>
+  `,
+  styles: ``
+})
+export class CommonsLibComponent {
+
+}
